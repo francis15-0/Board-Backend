7449c30c4992c32a8cc91835df1c97f8da90702e
francis15-0 <nnadiekwemakuo.11@gmail.com>
2025-12-16 03:40:34 -0500

    added functionality for login routes

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,85 @@
-import express from 'express'
-import pool from './db/db'
-import bcrypt from 'bcrypt'
-import cors from 'cors'
-const server = express()
+import express from "express";
+import pool from "./db/db";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import { RowDataPacket } from "mysql2";
+import jwt from "jsonwebtoken";
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 server.use(express.json());
 server.use(cors());
-const salt = 10
+const salt = 10;
+
+const jwt_secret: any = process.env.JWT_SECRET;
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  password_hash: string;
+}
 
 server.get("/", (req, res) => {
-    res.json({ message: "Server Running" })
-})
+  res.json({ message: "Server Running" });
+});
 
 server.post("/register", async (req, res) => {
-    try {
-        const { username, email, password } = req.body
+  try {
+    const { username, email, password } = req.body;
 
-        if (!username || !email || !password) {
-            return res.status(400).json({ message: "Email or password is empty" });
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Email or password is empty" });
+    } else {
+      const hash = await bcrypt.hash(password, salt);
+      const [rows]: any = await pool.query(
+        "INSERT INTO users (username, email, password_hash) VALUES (?, ?)",
+        [username, email, hash]
+      );
+      if (rows.insertId > 0) {
+        return res.json({ message: "Account created Succesfully" });
+      }
+    }
+  } catch (error) {
+    console.error("This is the error ", error);
+  }
+});
 
-        }
-        else {
-            const hash = await bcrypt.hash(password, salt)
-            const [row]: any = await pool.query("INSERT INTO users (username, email, password_hash) VALUES (?, ?)", [username, email, hash])
-            if (row.insertId > 0) {
-                return res.json({ message: "Account created Succesfully" });
-            }
-
-        }
-    } catch (error) {
-        console.error("This is the error ", error);
+server.post("/login", async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Theres no Email or password" });
     }
 
-})
+    const [rows] = await pool.query<(User & RowDataPacket)[]>(
+      `select id, password_hash, username FROM board where email = ?`,
+      [email]
+    );
 
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "That email does not exit" });
+    }
 
-server.post("/login", (req, res)=>{
-    
-})
+    const user = rows[0];
+    const ok = await bcrypt.compare(password, user.password_hash);
+    if (!ok) {
+      return res.status(401).json({ message: "wrong user name or password" });
+    }
 
+    const token = jwt.sign(
+      { userName: user.username, userId: user.id },
+      jwt_secret
+    );
 
+    return res.status(200).json({
+      token: token,
+      user: user.username,
+    });
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+});
 
-
-
-
-
-server.listen('3000', () => {
-    console.log("Server running on port 3000")
-})
+server.listen("3000", () => {
+  console.log("Server running on port 3000");
+});
