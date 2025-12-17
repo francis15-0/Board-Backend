79115eed9f23f6b4b8483af9ef6ad3062eec4d20
francis15-0 <nnadiekwemakuo.11@gmail.com>
2025-12-16 20:51:39 -0500

    Added code for board and task table

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,10 +1,9 @@
 import pool from "../db/db";
 
-
 async function initDatabase() {
-    try {
-        console.log("Initializing Database...")
-        await pool.query(`
+  try {
+    console.log("Initializing Database...");
+    await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(50) NOT NULL UNIQUE,
@@ -14,13 +13,37 @@ async function initDatabase() {
       )
     `);
 
-            
-        console.log("✅ user table ready")
-        
-    } catch (error) {
-        console.error('❌ Database initialization failed:', error);
-        throw error;
-    }
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS boards(
+      id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      user_id INT UNSIGNED NOT NULL,
+      tittle VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+      )
+      `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS tasks(
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      user_id INT UNSIGNED NOT NULL,
+      board_id INT UNSIGNED NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      description TEXT,
+      status ENUM('todo','doing','done') DEFAULT 'todo',
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE
+
+      )
+      
+      `);
+
+    console.log("✅ user table ready");
+  } catch (error) {
+    console.error("❌ Database initialization failed:", error);
+    throw error;
+  }
 }
 
-initDatabase()
+initDatabase();
